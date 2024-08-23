e50783d4f05de997edc48c6c1d07b68ae63e81b2
dahyun_oh <imcute0703123@naver.com>
2024-08-23 12:44:04 +0900

    2024/08/23

--- 단계별로 풀어보기/1/17009.py
+++ 단계별로 풀어보기/1/17009.py
@@ -1,14 +1,14 @@
-result = 0
-result1 = 0
+aresult = 0
+bresult = 0
 for i in range(3):
     a = int(input())
-    result += a
+    aresult += a * (3 - i)
 for i in range(3):
     b = int(input())
-    result1 += b
-if result > result1 :
-    print('A')
-if result < result1 :
+    bresult += b * (3 - i)
+if aresult < bresult:
     print('B')
-else :
+elif aresult > bresult:
+    print('A')
+else:
     print('T')
