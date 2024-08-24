c7a0d64b74bcf7eac269830775418dbb46a0d350
dahyun_oh <imcute0703123@naver.com>
2024-08-24 18:51:46 +0900

    2024/08/24

---
+++ 단계별로 풀어보기/1/1267.py
@@ -0,0 +1,8 @@
+g = int(input())
+for i in range(g):
+    s = int(input())
+if s < 30 :
+print('M')
+print('Y')
+print('M')
+print('Y')
---
+++ 단계별로 풀어보기/1/14581.py
@@ -0,0 +1,4 @@
+say = input()
+print(':fan::fan::fan:')
+print(':fan::'+say+'::fan:')
+print(':fan::fan::fan:')
